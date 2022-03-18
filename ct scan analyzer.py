80cfefccaf406faac1fccd6051745d213785e3f0
silyasm <78037911+silyasm@users.noreply.github.com>
2022-03-18 14:45:33 -0500

    Add files via upload

---
+++ ct scan analyzer.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage import io
+from skimage.feature import match_template
+
+
+image = io.imread("images/ct.scan.jpg")
+reference = io.imread("images/ct.scan.example.jpg")
+
+
+result = match_template(image, reference)
+ij = np.unravel_index(np.argmax(result), result.shape)
+x, y = ij[::-1]
+
+fig = plt.figure(figsize=(8, 3))
+ax1 = plt.subplot(1, 3, 1)
+ax2 = plt.subplot(1, 3, 2)
+ax3 = plt.subplot(1, 3, 3, sharex=ax2, sharey=ax2)
+
+ax1.imshow(reference, cmap=plt.cm.gray)
+ax1.set_axis_off()
+ax1.set_title('template')
+
+ax2.imshow(image, cmap=plt.cm.gray)
+ax2.set_axis_off()
+ax2.set_title('image')
+# highlight matched region
+hreference, wrefernce = reference.shape
+rect = plt.Rectangle((x, y), wrefernce, hreference, edgecolor='r', facecolor='none')
+ax2.add_patch(rect)
+
+ax3.imshow(result)
+ax3.set_axis_off()
+ax3.set_title('`match_template`\nresult')
+# highlight matched region
+ax3.autoscale(False)
+ax3.plot(x, y, 'o', markeredgecolor='r', markerfacecolor='none', markersize=10)
+
+plt.show()
